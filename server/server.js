6ead662c57555f91846e2ed9c400c907078dc317
Kasey Corra <kaseycorra@Kaseys-MacBook-Air.local>
2024-04-06 14:47:29 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 
-// app.use(express.static(path.join(__dirname, '../client/build')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname+'../client/build/index.html'));
-//   });
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'../client/build/index.html'));
+  });
 app.use(express.json());
 app.use(cors());
 app.use(express.json());
